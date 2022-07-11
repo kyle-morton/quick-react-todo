db8a182bfc9d90ebd4e7a626deae9e5414d6148e
Kyle Morton <kylemortondevelopment@gmail.com>
2022-07-11 11:40:43 -0500

    wip on button onClick

--- react-todo/src/index.js
+++ react-todo/src/index.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function ToDo(props) {
+    return (
+        <li>
+            {props.title}
+        </li>
+    );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function CreateButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Add To-Do
+        </button>
+    );
+}
+
+class ToDoList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: [
+                {
+                    title: 'ToDo1'
+                },
+                {
+                    title: 'ToDo2'
+                }
+            ]
+        };
+    }
+
+    create() {
+        
+    }
+
+    render() {
+        return (
+            <div>
+                To-Do List
+                <ol>
+                    { 
+                        this.state.todos.map(todo => {
+                            return <ToDo title={todo.title}></ToDo>;
+                        })
+                    }
+                </ol>
+            </div>
+        );
+    }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<ToDoList />);
