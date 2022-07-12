ffdd00e48eb0276e71811ac862ad5d37727938a6
Kyle Morton <kylemortondevelopment@gmail.com>
2022-07-11 20:38:07 -0500

    Adding logic for adding, completing, or deleting todos

--- react-todo/src/index.js
+++ react-todo/src/index.js
@@ -2,34 +2,55 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-function ToDo(props) {
-    return (
-        <li>
-            {props.title}
-        </li>
-    );
-}
+class ToDo extends React.Component {
 
-function CreateButton(props) {
-    return (
-        <button onClick={props.onClick}>
-            Add To-Do
-        </button>
-    );
+    onDelete(event) {
+        this.props.onDelete(event, this.props.todo.id);
+        event.preventDefault();
+    }
+
+    onComplete(event) { 
+        this.props.onComplete(event, this.props.todo.id);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <li className={this.props.todo.isComplete ? 'complete' : ''}>
+                {this.props.todo.title} 
+                <button className="to-do-button" onClick={(e) => this.onDelete(e)}>Delete</button>
+                {!this.props.todo.isComplete &&
+                    <button className="to-do-button" onClick={(e) => this.onComplete(e)}>Complete</button>
+                }
+            </li>
+        );
+    }
 }
 
 class CreateForm extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: ''
+        };
+    }
 
     handleChange(event) {
         this.setState({title: event.target.value});
     }
 
+    submit(event) {
+        event.preventDefault();
+        this.props.handleCreate(this.state.title);
+    }
+
     render(props) {
         return (
-            <form onSubmit={this.props.handleCreate}>
+            <form onSubmit={ (e) => this.submit(e)}>
             <label>
               Title:
-              <input type="text" value={this.state.title} onChange={this.handleChange} />
+              <input type="text" value={this.state.title} onChange={(e) => this.handleChange(e)} />
             </label>
             <input type="submit" value="Add To-Do" />
           </form>
@@ -43,33 +64,68 @@ class ToDoList extends React.Component {
         this.state = {
             todos: [
                 {
-                    title: 'ToDo1'
+                    title: 'ToDo1',
+                    id: 1
                 },
                 {
-                    title: 'ToDo2'
+                    title: 'ToDo2',
+                    id: 2
                 }
             ]
         };
     }
 
-    create() {
+    createToDo(name) {
         let todoList = this.state.todos.slice();
-        todoList.push({ title: 'ToDo' + (todoList.length+1) });
+        todoList.push({ 
+            title: name, 
+            id: ((new Date().getTime() * 10000) + 621355968000000000)
+        });
         this.setState({
             todos: todoList
         });
+    }
+
+    deleteToDo(event, id) {
+        let todos = this.state.todos.slice().filter(e => {
+            if (e.id != id) {
+                return e;
+            }
+        });
+
+        this.setState({
+            todos: todos
+        })
+    }
+
+    completeToDo(event, id) {
+        let todos = this.state.todos.slice();
+        var todo = todos.find(td => {
+            if (td.id == id) {
+                return td;
+            }
+        });
+
+        if (!todo) {
+            return;
+        }
+
+        todo.isComplete = true;
+
+        this.setState({
+            todos: todos
+        })
     }
 
     render() {
         return (
             <div>
                 <h3>To-Do List</h3>
-                <CreateForm handleCreate={(i) => this.create(i)}></CreateForm>
-                {/* <CreateButton onClick={(i) => this.create(i)}></CreateButton> */}
+                <CreateForm handleCreate={(i) => this.createToDo(i)}></CreateForm>
                 <ol>
                     { 
-                        this.state.todos.map(todo => {
-                            return <ToDo title={todo.title}></ToDo>;
+                        this.state.todos.map((todo, index) => {
+                            return <ToDo key={todo.id} todo={todo} onDelete={this.deleteToDo.bind(this)} onComplete={this.completeToDo.bind(this)}></ToDo>;
                         })
                     }
                 </ol>
