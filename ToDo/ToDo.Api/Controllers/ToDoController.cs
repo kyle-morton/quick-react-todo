c90a44fcf4001e46e4a4552418f1409888856fa6
Kyle Morton <kylemortondevelopment@gmail.com>
2022-07-11 21:16:06 -0500

    Adding in mock controller for todos

---
+++ ToDo/ToDo.Api/Controllers/ToDoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDo.Api.Models;
+
+namespace ToDo.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ToDoController : ControllerBase
+{
+    public ToDoController()
+    {
+    }
+
+    [HttpGet(Name = "GetToDos")]
+    public IEnumerable<ToDoItem> Get()
+    {
+        return new List<ToDoItem>
+        {
+            new ToDoItem
+            {
+                Id = (int)DateTime.Now.Ticks,
+                Title = "Replace Lightbulb",
+                IsComplete = true
+            },
+            new ToDoItem
+            {
+                Id = (int)DateTime.Now.Ticks,
+                Title = "Buy Milk"
+            }
+        };
+    }
+}
+
---
+++ ToDo/ToDo.Api/Models/ToDoItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ToDo.Api.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
+
